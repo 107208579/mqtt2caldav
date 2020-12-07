b1bffa7c43af784d71fdb7d04c056a8fed1caf54
starpolar <highfamous@outlook.com>
2020-12-07 21:56:04 +0300

    time update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import paho.mqtt.client as mqttClient
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 import caldav
 from utils.constants import *
@@ -23,27 +23,37 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, message):
     logger.info("Message received : " + str(message.payload) + " on " + message.topic)
     print("Message received : " + str(message.payload) + " on " + message.topic)
-    my_new_calendar = my_principal.make_calendar(name="Calendar")
+    # my_new_calendar = my_principal.make_calendar(name="Calendar")
+    mqtt_event = str(message.payload).decode('ASCII')
     for trigger in TRIGGERS:
-        if trigger['MQTT_TOPIC'] == message.topic:
+        if trigger['MQTT_TOPIC'] == message.topic and trigger['MQTT_EVENT'] == mqtt_event:
+            now_datetime = datetime.now()
+            end_datetime = now_datetime + timedelta(minutes=int(trigger['EVENT_DURATION']))
+            if trigger['EVENT_SECONDS'] == 'False' or trigger['EVENT_SECONDS'] == 'false':
+                start_time = now_datetime.strftime('%Y%m%dT%H%M00')
+                end_time = end_datetime.strftime('%Y%m%dT%H%M00')
+            else:
+                start_time = now_datetime.strftime('%Y%m%dT%H%M%S')
+                end_time = end_datetime.strftime('%Y%m%dT%H%M%S')
+            event_calendar = caldav.Calendar(client=cal_client, url=trigger['EVENT_CALENDAR'])
             str_event = """BEGIN:VCALENDAR
             VERSION:2.0
             PRODID:-//Script//EN
             CALSCALE:GREGORIAN
             BEGIN:VEVENT
-            DTSTART;TZID=Asia/Singapore:20201125T220000
-            DTEND;TZID=Asia/Singapore:20201125T230000
+            DTSTART;TZID={timezone}:{start_time}
+            DTEND;TZID={timezone}:{end_time}
             UID:D04A88A5-A56A-4FC9-BEDF-A064C18EEB83
-            DTSTAMP:20201125T140000Z
-            LOCATION:12 Main Road\, 7344 Nowhere\, Alaska
-            DESCRIPTION:Meeting to discuss new product launches
-            URL;VALUE=URI:http://example.com
-            SUMMARY:Meeting Awesome Inc
-            GEO:48.85299;2.36885
-            CATEGORY:Meeting
-            CREATED:20201125T140000Z
+            DTSTAMP:{start_time}
+            LOCATION:{location}
+            DESCRIPTION:{description}
+            URL;VALUE=URI:{url}
+            SUMMARY:{summary}
+            GEO:{geo}
+            CATEGORY:{category}
+            CREATED:{created_time}
             BEGIN:VALARM
-            TRIGGER:-PT15M
+            TRIGGER:-PT{trigger_time}M
             ATTACH;VALUE=URI:Chord
             ACTION:AUDIO
             END:VALARM
@@ -51,15 +61,17 @@ def on_message(client, userdata, message):
             END:VCALENDAR
             """
             str_event = str_event.format(timezone=trigger['EVENT_TIMEZONE'], location=trigger['EVENT_LOCATION'], description=trigger['EVENT_DESCRIPTION'], url=trigger['EVENT_URL'],
-                                         summary=trigger['EVENT_SUMMARY'], geo=trigger['EVENT_GEO'], category=trigger['EVENT_CATEGORY'], trigger_time=trigger['EVENT_TRIGGER'])
+                                         summary=trigger['EVENT_SUMMARY'], geo=trigger['EVENT_GEO'], category=trigger['EVENT_CATEGORY'], created_time=start_time, trigger_time=trigger['EVENT_TRIGGER'],
+                                         start_time=start_time, end_time=end_time)
             # Let's add an event to our newly created calendar
-            my_event = my_new_calendar.save_event(str_event)
+            my_event = event_calendar.save_event(str_event)
 
 
 if __name__ == '__main__':
     # CALDAV Server Connection
-    client = caldav.DAVClient(url=CALDAV_SERVER_ADDRESS, username=CALDAV_USERNAME, password=CALDAV_PASSWORD)
-    my_principal = client.principal()
+    global cal_client
+    cal_client = caldav.DAVClient(url=CALDAV_SERVER_ADDRESS, username=CALDAV_USERNAME, password=CALDAV_PASSWORD)
+    my_principal = cal_client.principal()
     calendars = my_principal.calendars()
     if calendars:
         print("your principal has %i calendars:" % len(calendars))
@@ -69,12 +81,12 @@ if __name__ == '__main__':
         print("your principal has no calendars")
 
     # MQTT Broker Connection
-    client = mqttClient.Client("Python")
-    client.username_pw_set(MQTT_USERNAME, password=MQTT_PASSWORD)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(MQTT_SERVER_ADDRESS, port=int(MQTT_SERVER_PORT))
-    client.loop_start()
+    mqtt_client = mqttClient.Client("Python")
+    mqtt_client.username_pw_set(MQTT_USERNAME, password=MQTT_PASSWORD)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(MQTT_SERVER_ADDRESS, port=int(MQTT_SERVER_PORT))
+    mqtt_client.loop_start()
 
     try:
         while True:
@@ -83,5 +95,5 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         logger.warn("exiting")
         print("exiting")
-        client.disconnect()
-        client.loop_stop()
+        mqtt_client.disconnect()
+        mqtt_client.loop_stop()
